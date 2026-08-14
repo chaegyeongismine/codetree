b27c82b0d1460fc17e0517ec5d4f7821ab540483
chaegyeongismine <000jjy@naver.com>
2026-08-15 05:05:10 +0900

    [Passed] 부분문자열 위치 구하기 (116ms, 10MB)

---
+++ trail1/부분문자열 위치 구하기/find-location-of-substring.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String a = sc.next();
+        int idx = str.indexOf(a);
+        System.out.print(idx);
+        // Please write your code here.
+    }
+}
