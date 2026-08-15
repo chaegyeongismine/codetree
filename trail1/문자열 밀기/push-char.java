73ef6ad82832595b3dc294349225cfb66b36ea92
chaegyeongismine <000jjy@naver.com>
2026-08-16 04:29:16 +0900

    [Passed] 문자열 밀기 (120ms, 10MB)

---
+++ trail1/문자열 밀기/push-char.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        str = str.substring(1) + str.charAt(0);
+        System.out.print(str);
+        // Please write your code here.
+    }
+}
