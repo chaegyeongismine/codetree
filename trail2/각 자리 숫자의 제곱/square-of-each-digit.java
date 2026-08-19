c90bb7f43660e427fa3e91ecf84d045599244eff
chaegyeongismine <000jjy@naver.com>
2026-08-19 19:58:07 +0900

    [Passed] 각 자리 숫자의 제곱 (127ms, 11MB)

---
+++ trail2/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static int gak(int n){
+        if(n<10) return n*n;
+    
+        return  gak(n/10) + (n%10)*(n%10);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(gak(n));
+        // Please write your code here.
+    }
+}
