ce0b5262f432128afcc078ea80d164267628f45d
chaegyeongismine <000jjy@naver.com>
2026-08-20 15:09:11 +0900

    [Passed] 오름 내림차순 정렬 (174ms, 12MB)

---
+++ trail2/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i =0; i<arr.length; i++)
+        System.out.print(arr[i]+" ");
+        System.out.println();
+        for(int i =arr.length -1; i>=0; i--)
+        System.out.print(arr[i]+" ");
+        // Please write your code here.
+    }
+}
