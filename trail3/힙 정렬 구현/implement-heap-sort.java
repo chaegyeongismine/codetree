98808dc9edbd5e168e706a4d915ed4f9117eeb57
chaegyeongismine <000jjy@naver.com>
2026-09-02 01:12:14 +0900

    [Passed] 힙 정렬 구현 (774ms, 32MB)

---
+++ trail3/힙 정렬 구현/implement-heap-sort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+    public static void swap(int[] arr, int idx1, int idx2){
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
+    public static void heapify(int[] arr, int n,  int idx){
+        int largest = idx;
+        int left = 2*idx ;
+        int right = 2*idx + 1;
+
+        if(left <=n && arr[left]>arr[largest])
+            largest = left;
+
+        if(right<=n && arr[right]>arr[largest])
+            largest = right;
+
+        if(largest != idx) {
+            swap(arr, idx, largest);
+            heapify(arr, n, largest);
+        } 
+        }
+    public static void heap_sort(int[] arr){
+        int n = arr.length-1;
+        for(int i=n/2; i>=1; i--) heapify(arr,n, i);
+        for(int i = n; i>1; i--){
+        swap(arr, 1, i);
+        heapify(arr, i-1, 1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        heap_sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.print(sb);
+        }
+}
