60992c051be6edbc0ed5175f3521ae193c560ade
chaegyeongismine <000jjy@naver.com>
2026-09-01 20:30:36 +0900

    [Passed] 황금비율 토스트 (356ms, 18MB)

--- trail3/황금비율 토스트/golden-toast.java
+++ trail3/황금비율 토스트/golden-toast.java
@@ -1,50 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.ListIterator;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        
-        String s = br.readLine();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         LinkedList<Character> l = new LinkedList<>();
-        for (int i = 0; i < s.length(); i++) {
-            l.add(s.charAt(i));
-        }
         
-        // 커서를 리스트 맨 뒤에 배치 ($O(1)$)
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        String s = sc.next();
+        for(int i = 0; i<s.length(); i++) l.add(s.charAt(i)); 
         ListIterator<Character> it = l.listIterator(l.size());
+        sc.nextLine();
         
         for (int i = 0; i < m; i++) {
-            String command = br.readLine();
-            char c = command.charAt(0);
-            
-            if (c == 'L') {
-                if (it.hasPrevious()) it.previous();
-            } else if (c == 'R') {
-                if (it.hasNext()) it.next();
-            } else if (c == 'D') {
-                if (it.hasNext()) {
-                    it.next();
-                    it.remove();
-                }
-            } else if (c == 'P') {
-                it.add(command.charAt(2));
+            String command = sc.next();
+            if(command.equals("L")) {
+                if(it.hasPrevious()) it.previous();
             }
+            else if(command.equals("R")){
+                if(it.hasNext()) it.next();
+            }
+            else if(command.equals("D") && it.hasNext()) {
+                it.next();
+                it.remove();
+            }
+            else if(command.charAt(0)=='P') it.add(sc.next().charAt(0));
         }
-        
-        // StringBuilder에 결과를 전부 담아 한 번에 출력
-        StringBuilder sb = new StringBuilder();
-        for (char ch : l) {
-            sb.append(ch);
+        it = l.listIterator();
+        while(it.hasNext()) {
+            System.out.print(it.next());
         }
-        System.out.println(sb.toString());
+        // Please write your code here.
     }
 }
