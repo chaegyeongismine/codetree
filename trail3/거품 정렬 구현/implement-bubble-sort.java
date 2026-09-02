fe66814cd96061473ced88e613f0b8b1a427c0f3
chaegyeongismine <000jjy@naver.com>
2026-09-02 22:21:28 +0900

    [Passed] 거품 정렬 구현 (136ms, 10MB)

---
+++ trail3/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void swap(int[] a , int idx1, int idx2){
+        int temp = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        for(int i=0; i<n-1;i++){
+            for(int j=0; j<n-1-i; j++){
+                if(arr[j]>arr[j+1]) swap(arr, j, j+1);
+            }
+        }
+        for(int i=0; i<n; i++) System.out.print(""+arr[i]+" ");
+        // Please write your code here.
+    }
+}
